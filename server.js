026ac6329f7df07172ae54802567f6cec6244dcb
Luis G <luisdavidnazareth@gmail.com>
2024-10-18 19:43:05 -0300

    eliminacion de comentario

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
-import express from "express";//Empezar a utilizar express esta en el carpeta node_modules
-import "dotenv/config.js"//Importamos la libreria dotenv
-import "./config/database.js"// ojo con las importaciones el orden es muy importante.
+import express from "express";
+import "dotenv/config.js"
+import "./config/database.js"
 import cors from 'cors'
 import morgan from 'morgan'
 import indexRouter from "./router/index.js"
@@ -9,21 +9,21 @@ import bad_request from "./middlewares/bad_request.js";
 import error_handler from "./middlewares/error_handler.js";
 
 
-const server = express() // Creamos una variable por buenas practicas se le coloca el nombre server que contiene todo el express()
+const server = express()
 
-const PORT = process.env.PORT || 8080 //Creamos una variable donde se va a correr nuestra aplicacion en el Backend se utiliza el puerto 8080 pero esta esta guardada en el archivo .env
+const PORT = process.env.PORT || 8080 
 
 const ready = () => console.log("server ready in port = "+PORT);
 
-//Configuramos cuatros middlewares 
 
-server.use(express.json())// se utiliza para responder o recibir en formato json
 
-server.use(express.urlencoded({ extended: true}))// se utiliza para poder recibir parametros y querys
+server.use(express.json())
+
+server.use(express.urlencoded({ extended: true}))
 
 server.use(cors())
 
-server.use(morgan('dev'))//morgan se utilizar para controlar la peticiones que nos hagan
+server.use(morgan('dev'))
 
 
 //router 
@@ -35,7 +35,7 @@ server.use(error_handler)
 
 
 
-server.listen(PORT,ready)//Levantamos el servidor, utilizamos la propiedad listen para escucha, el puerto 8080. 
+server.listen(PORT,ready)
 
 
 
